dc39fa1332392ad848c0ed5dd7abca308d34e097
AlexMaderaP <alejandro.maderap@gmail.com>
2022-04-20 13:12:52 -0500

    Agregrando pasaos 2 al 4

---
+++ index.js
@@ -0,0 +1,9 @@
+const MissionCommander = require('./app/missionCommander');
+
+const carlo = new MissionCommander("Carlo")
+const rodrigo = new MissionCommander("Rodrigro")
+const perla = new MissionCommander("Perla")
+
+console.log(carlo.name)
+console.log(rodrigo.name)
+console.log(perla.name)
